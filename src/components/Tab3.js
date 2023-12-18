5cab9480e1c02a518fa4c07b4d6c088ccd7a0573
teeshaduseja81 <146194727+teeshaduseja81@users.noreply.github.com>
2023-12-19 00:24:22 +0500

    Create Tab3.js
    tab3

---
+++ src/components/Tab3.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { placesStore } from '../store/Store'
+import "../styles/Tab1.css";
+import { AiFillStar } from "react-icons/ai"
+import { Link } from 'react-router-dom';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/bundle';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { FiHeart } from "react-icons/fi"
+import { IoBedOutline } from "react-icons/io5"
+import { GiBathtub } from "react-icons/gi"
+import { BsStars } from "react-icons/bs"
+import { BiBuildingHouse } from "react-icons/bi"
+import { Pagination } from "swiper";
