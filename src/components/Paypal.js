ce2c0876367b070cfe43a20fdb462d75709badc6
teeshaduseja81 <146194727+teeshaduseja81@users.noreply.github.com>
2023-12-18 23:36:08 +0500

    Create Paypal.js
    Paypal section

---
+++ src/components/Paypal.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import "../styles/Paypal.css";
+import paypalbg from "../styles/paypalbg.png"
+import paypalCard from "../styles/paypal-bgg.png"
+import success from "../styles/ab.gif";
+import { PayPalButtons } from '@paypal/react-paypal-js';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+
