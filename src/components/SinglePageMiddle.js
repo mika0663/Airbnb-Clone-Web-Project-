0c655d60bfb7efd509a84844ecbba9e9034df8fb
teeshaduseja81 <146194727+teeshaduseja81@users.noreply.github.com>
2023-12-19 00:08:12 +0500

    Create SinglePageMiddle.js
    single middle page

---
+++ src/components/SinglePageMiddle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/SinglePageMiddle.css"
+import section3 from "../../src/styles/av.png"
+import { useParams } from 'react-router-dom'
+import { FaStar } from "react-icons/fa"
+import { placesStore } from '../store/Store'
+import rev1 from "../styles/stars1.png"
+import rev2 from "../styles/stars2.png"
+import reviews from "../styles/rev.png";
+import modalPic from "../styles/pic.png"
+
+
